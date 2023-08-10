9be15b1115ae87b0d35d12b0f566ae959342f738
ChanWengHin <139675606+ChanWengHin@users.noreply.github.com>
2023-08-10 22:06:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import overheads, cash_on_hand, profit_loss
 def main():
     from pathlib import Path
     # create a path object "file_path" which contains the file path to the new file "summary_report.txt" that I want to create
-    file_path= Path.cwd()/"project_group"/"summary_report.txt"
+    file_path= Path.cwd()/"summary_report.txt"
     file_path.touch()
     overheads.overhead_function()
     cash_on_hand.coh_function()
