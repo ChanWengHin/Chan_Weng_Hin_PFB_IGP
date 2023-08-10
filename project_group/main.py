ee239c7fb87cf52095bd89d2a4c0a29f7502c127
Connie <139679586+Connie-G@users.noreply.github.com>
2023-08-10 12:38:07 +0800

    Update main.py

--- project_group/main.py
+++ project_group/main.py
@@ -1,1 +1,11 @@
+#import the "overheads", "cash_on_hand" and "profit_loss" modules
+import overheads, cash_on_hand, profit_loss
 
+#writing custom fuction called "main()" to carry out the functions in the three imported modules
+def main():
+    overheads.overhead_function()
+    cash_on_hand.coh_function()
+    profit_loss.profitloss_function()
+
+#activate the "main()" function
+main()
